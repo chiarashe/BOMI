b1e53958f5b2c0f94f4035a720096c5556c0c731
Chiara Sheldon <chiarasheldon@gmail.com>
2024-02-01 15:05:45 -0600

    user model and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  ROLES = %w(Patient Doctor)
+  validates :role, inclusion: { in: ROLES }
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true, uniqueness: true
+
+  has_many :reports, dependent: :destroy
+  has_one_attached :photo
+
+  has_many :doctors, dependent: :destroy
+  has_many :patients, through: :reports, source: :reports, dependent: :destroy
+
+  def patient?
+    role == "Patient"
+  end
+
+  def doctor?
+    role == "Doctor"
+  end
 end
